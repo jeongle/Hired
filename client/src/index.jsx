b6459323f7f38dba853e78d356933f70d8de68e3
jeongle <jyjjeong@gmail.com>
2018-10-13 11:44:41 -0700

    react components initialized

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,5 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./main/App.js";
+import App from "../main/App.jsx";
 
 ReactDOM.render(<App />, document.getElementById("hired"));
