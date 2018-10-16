38802eaecf27886ea72f150f4f81ce43c590f073
jeongle <jyjjeong@gmail.com>
2018-10-16 00:25:15 -0700

    Done with buttons

--- client/main/Job.jsx
+++ client/main/Job.jsx
@@ -4,17 +4,21 @@ const Job = (props) => {
   return (
     <div className="Job">
       <div className="title">
-        <button onClick={() => props.removeApp(props.section, props.index)}>X</button>
-        {props.todo.jobTitle}
+        <div>{props.todo.jobTitle}</div>
+        <button 
+          className="delete"
+          onClick={() => props.removeApp(props.section, props.index)}>
+          X
+        </button>
       </div>
       <div className="company">
-        {props.todo.company}
+        Company: {props.todo.company}
       </div>
       <div className="location">
-        {props.todo.location}
+        Location: {props.todo.location}
       </div>
       <div className="url">
-        {props.todo.url}
+        <a target="_blank" href={`https://www.${props.todo.url}` }>Link</a>
       </div>
     </div>
   )
